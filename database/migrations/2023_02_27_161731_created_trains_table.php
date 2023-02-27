487cc5d4dce869ef26344dfbae5086a05f0332a1
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-27 18:15:03 +0100

    created trains table migration

---
+++ database/migrations/2023_02_27_161731_created_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 30);
+            $table->string('stazione_di_partenza', 30);
+            $table->string('stazione_di_arrivo', 30);
+            $table->time('oraio_di_partenza');
+            $table->time('oraio_di_arrivo');
+            $table->smallInteger('codice_treno')->unsigned();
+            $table->tinyInteger('numero_carozze')->unsigned(); 
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
